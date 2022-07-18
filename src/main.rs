307817b43cf7c53db73d57860fffb946cad1148b
160R <160R@protonmail.com>
2022-07-18 03:23:34 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -44,100 +44,40 @@ impl LibinputInterface for Interface {
 }
 
 
+macro_rules! dispatch {
+    ($libinput: ident) => {
+        $libinput.dispatch()
+            .unwrap()
+    };
+}
+
+
 fn main() {
     let terminate = Arc::new(AtomicBool::default());
 
-    for sig in signal_hook::consts::TERM_SIGNALS {
-
-        let sig_handler = Arc::clone(&terminate);
-        signal_hook::flag::register(*sig, sig_handler)
-            .unwrap();
-    }
-
+    _register_signal_handling_(&terminate);
 
     let mut conn = swayipc::Connection::new()
         .expect("Cannot connect to Sway!");
 
-
-    let args = Args::parse();
-    println!("{args:#?}");
-
-    let Args {
-        device,
-        r#type,
-        pointer_acceleration,
-        javelin_acceleration,
-        pointer_cooldown,
-        javelin_cooldown,
-        reload_msec,
-        tremble_msec,
-        x_split_reload,
-        y_split_reload,
-        offsets
-    } = args;
-
-
-    let offsets: std::collections::HashMap<_, _> = offsets.iter()
-        .map(|s| {
-            let mut it = s.split(':');
-            let app = it.next()
-                .unwrap();
-            let offsets = (
-                it.next()
-                    .unwrap_or("0")
-                    .parse::<i32>()
-                    .expect("Invalid x offset format"),
-                it.next()
-                    .unwrap_or("0")
-                    .parse::<i32>()
-                    .expect("Invalid y offset format"),
-            );
-            (app, offsets)
-        })
-        .collect();
-
-    let device_type = r#type
-        .unwrap_or("touchpad".to_string());
-
+    let (
+        offsets,
+        device_path,
+        device_type,
+        Args {
+            pointer_acceleration,
+            javelin_acceleration,
+            pointer_cooldown,
+            javelin_cooldown,
+            reload_msec,
+            tremble_msec,
+            x_split_reload,
+            y_split_reload,
+            ..
+        }
+    ) = get_arguments();
 
     let mut libinput = Libinput::new_from_path(Interface);
-
-
-    let device_path = device.unwrap_or_else(|| {
-
-        let mut event_mouse_devices = vec![];
-
-        for entry in
-            std::fs::read_dir("/dev/input/by-path")
-                .expect("Cannot inspect /dev/input/by-path directory")
-                .map(|p| p
-                    .expect("Cannot inspect some input device")
-                )
-        {
-            let device_path = entry
-                .path()
-                .to_string_lossy()
-                .to_string();
-
-            if device_path.contains("event-mouse") {
-                event_mouse_devices
-                    .push(device_path)
-            }
-        }
-
-        event_mouse_devices
-            .sort();
-
-        println!("The following pointer devices were found: {event_mouse_devices:#?}");
-
-        if event_mouse_devices.len() < 2 {
-            eprintln!("Javelin could be annoying with only one pointer device available")
-        }
-
-        event_mouse_devices
-            .pop()
-            .expect("No pointer devices were found")
-    });
 
     let pointer_device = libinput
         .path_add_device(&device_path)
@@ -157,54 +97,36 @@ fn main() {
     let mut javelin = true;
 
     let mut past_tremble_time = 0;
-    let mut tremble = true;
-    let mut trembles = [
-        15, 8, 10, 9, 16, 7, 18, 13, 6, 11, 19, 12, 17
-    ]
-        .into_iter()
-        .scan((0..8).cycle(), |dir, dist| {
-            match dir.next().unwrap() {
-                0 => Some([(0, dist), (dist, 0), (0, -dist), (-dist, 0), (0, 0)]),
-                1 => Some([(-dist, 0), (0, -dist), (dist, 0), (0, dist), (0, 0)]),
-                2 => Some([(0, dist), (-dist, 0), (0, -dist), (dist, 0), (0, 0)]),
-                3 => Some([(dist, 0), (0, -dist), (-dist, 0), (0, dist), (0, 0)]),
-                4 => Some([(dist, 0), (0, dist), (-dist, 0), (0, -dist), (0, 0)]),
-                5 => Some([(0, -dist), (-dist, 0), (0, dist), (dist, 0), (0, 0)]),
-                6 => Some([(-dist, 0), (0, dist), (dist, 0), (0, -dist), (0, 0)]),
-                7 => Some([(0, -dist), (dist, 0), (0, dist), (-dist, 0), (0, 0)]),
-                _ => unreachable!()
-            }
-        })
-        .flat_map(|x| x)
-        .cycle();
+    let mut trembling = true;
+    let mut trembles = trembles();
+
+    let mut tremble = || trembles
+        .next()
+        .unwrap();
 
 
     loop {
-        libinput
-            .dispatch()
-            .unwrap();
+        dispatch!(libinput);
 
         let event = match libinput.next() {
             Some(input::Event::Pointer(ev)) => ev,
             _ => {
-                if tremble {
+                if trembling {
 
                     let mut wait_times = tremble_msec / (tremble_msec / 6);
 
                     while wait_times > 0 {
 
-                        libinput
-                            .dispatch()
-                            .unwrap();
+                        dispatch!(libinput);
+
                         spin_sleep::sleep(std::time::Duration::from_millis(6 as u64));
 
                         wait_times -= 1;
                     }
 
-                    let (x, y) = trembles.next()
-                        .unwrap();
+                    let (x, y) = tremble();
                     if (x, y) == (0, 0) {
-                        tremble = false;
+                        trembling = false;
                     } else {
                         conn.run_command(format!("seat seat0 cursor move {x} {y}"))
                             .unwrap();
@@ -221,7 +143,7 @@ fn main() {
                     "))
                     .unwrap();
 
-                    return
+                    std::process::exit(0)
                 }
 
                 continue
@@ -250,9 +172,7 @@ fn main() {
                     "))
                     .unwrap();
 
-                    libinput
-                        .dispatch()
-                        .unwrap();
+                    dispatch!(libinput);
 
                     let focused_window = conn
                         .get_tree()
@@ -282,9 +202,7 @@ fn main() {
                         y += y_offset;
                     }
 
-                    libinput
-                        .dispatch()
-                        .unwrap();
+                    dispatch!(libinput);
 
                     conn.run_command(format!("
                         seat seat0 cursor set {x} {y}
@@ -292,7 +210,7 @@ fn main() {
                     .unwrap();
 
                     javelin = true;
-                    tremble = true;
+                    trembling = true;
 
                     continue
                 }
@@ -315,14 +233,14 @@ fn main() {
                     .unwrap();
 
                     javelin = true;
-                    tremble = true;
+                    trembling = true;
 
                     continue
                 }
 
                 if javelin {
                     let delta_time = current_event_time.saturating_sub(past_tremble_time);
-                    tremble = true;
+                    trembling = true;
 
                     if delta_time < tremble_msec {
                         continue
@@ -330,11 +248,9 @@ fn main() {
 
                     past_tremble_time = current_event_time;
 
-                    let (mut x, mut y) = trembles.next()
-                        .unwrap();
+                    let (mut x, mut y) = tremble();
                     if (x, y) == (0, 0) {
-                        (x, y) = trembles.next()
-                            .unwrap();
+                        (x, y) = tremble();
                     }
 
                     conn.run_command(format!("seat seat0 cursor move {x} {y}"))
@@ -355,14 +271,122 @@ fn main() {
 }
 
 
+fn _register_signal_handling_(terminate: &Arc<AtomicBool>) {
+    for sig in signal_hook::consts::TERM_SIGNALS {
+
+        let sig_handler = Arc::clone(terminate);
+        signal_hook::flag::register(*sig, sig_handler)
+            .unwrap();
+    }
+}
+
+
+type Offsets = std::collections::HashMap<String, (i32, i32)>;
+type DevicePath = String;
+type DeviceType = String;
+
+fn get_arguments() -> (Offsets, DevicePath, DeviceType, Args) {
+    let args = Args::parse();
+    println!("{args:#?}");
+
+    let offsets: Offsets = args.offsets.iter()
+        .map(|s| {
+            let mut app_x_y = s.split(':');
+
+            let app = app_x_y.next()
+                .unwrap()
+                .to_string();
+
+            let offsets = (
+                app_x_y.next()
+                    .unwrap_or("0")
+                    .parse::<i32>()
+                    .expect("Invalid x offset format"),
+                app_x_y.next()
+                    .unwrap_or("0")
+                    .parse::<i32>()
+                    .expect("Invalid y offset format"),
+            );
+            (app, offsets)
+        })
+        .collect();
+
+    let device_type = args.device_type
+        .clone()
+        .unwrap_or("touchpad".to_string());
+
+    let device_path = args.device
+        .clone()
+        .unwrap_or_else(|| {
+            let mut event_mouse_devices = vec![];
+
+            for entry in
+                std::fs::read_dir("/dev/input/by-path")
+                    .expect("Cannot inspect /dev/input/by-path directory")
+                    .map(|p| p
+                        .expect("Cannot inspect some input device")
+                    )
+            {
+                let device_path = entry
+                    .path()
+                    .to_string_lossy()
+                    .to_string();
+
+                if device_path.contains("event-mouse") {
+                    event_mouse_devices
+                        .push(device_path)
+                }
+            }
+
+            event_mouse_devices
+                .sort();
+
+            println!("The following pointer devices were found: {event_mouse_devices:#?}");
+
+            if event_mouse_devices.len() < 2 {
+                eprintln!("Javelin could be annoying with only one pointer device available")
+            }
+
+            event_mouse_devices
+                .pop()
+                .expect("No pointer devices were found")
+        });
+
+    (offsets, device_path, device_type, args)
+}
+
+
+fn trembles() -> impl Iterator<Item = (i32, i32)> {
+    [
+        15, 8, 10, 9, 16, 7, 18, 13, 6, 11, 19, 12, 17
+    ]
+        .into_iter()
+        .scan((0..8).cycle(), |dir, dist| {
+            match dir.next().unwrap() {
+                0 => Some([(0, dist), (dist, 0), (0, -dist), (-dist, 0), (0, 0)]),
+                1 => Some([(-dist, 0), (0, -dist), (dist, 0), (0, dist), (0, 0)]),
+                2 => Some([(0, dist), (-dist, 0), (0, -dist), (dist, 0), (0, 0)]),
+                3 => Some([(dist, 0), (0, -dist), (-dist, 0), (0, dist), (0, 0)]),
+                4 => Some([(dist, 0), (0, dist), (-dist, 0), (0, -dist), (0, 0)]),
+                5 => Some([(0, -dist), (-dist, 0), (0, dist), (dist, 0), (0, 0)]),
+                6 => Some([(-dist, 0), (0, dist), (dist, 0), (0, -dist), (0, 0)]),
+                7 => Some([(0, -dist), (dist, 0), (0, dist), (-dist, 0), (0, 0)]),
+                _ => unreachable!()
+            }
+        })
+        .flat_map(|x| x)
+        .cycle()
+}
+
+
 #[derive(Parser, Debug)]
 #[clap(author, version, global_setting = AppSettings::DeriveDisplayOrder)]
 struct Args {
     #[clap(display_order=0, long, requires = "type")]
     device: Option<String>,
 
-    #[clap(display_order=0, long)]
-    r#type: Option<String>,
+    #[clap(display_order=0, name = "type")]
+    device_type: Option<String>,
 
     #[clap(display_order=0, long, default_value = "-0.2")]
     pointer_acceleration: f32,
